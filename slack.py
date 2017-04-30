9b5c06fa29aba6d0fed93d3f73e1c459989eca97
Chiemeka Alim <lord.chiemy@gmail.com>
2017-04-30 23:16:08 +0100

    Testing cron job functionality

--- slack.py
+++ slack.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+from threading import Thread
 
+from datetime import datetime, timedelta
+import time
 import lunchbot
 
 lunchBot = lunchbot.Bot()
+jobs = []
 # slack = lunchBot.client
 
 app = Flask(__name__)
@@ -16,6 +20,7 @@ def index():
 
 @app.route("/install")
 def oauth_dance():
+    global jobs
     print('in oauth')
     """
     Renders installation page with "Add to Slack" button
@@ -24,11 +29,19 @@ def oauth_dance():
     print('code', code)
     if code:
         lunchBot.auth(code)
+        if lunchBot in jobs:
+            # Team already registered
+            print('Team already registered')
+            return redirect(url_for('thanks'))
+        print('about to append to jobs: ', jobs)
+        jobs.append(lunchBot)
+        print('just appended this to jobs: ', lunchBot)
+        print('jobs: ', jobs)
         return redirect(url_for('thanks'))
 
     print('in except')
-    print(lunchBot.oauth)
-    print(dir(lunchBot))
+    # print(lunchBot.oauth)
+    # print(dir(lunchBot))
     client_id = lunchBot.oauth["client_id"]
     print('client_id: ', client_id)
     scope = lunchBot.oauth["scope"]
@@ -36,11 +49,53 @@ def oauth_dance():
     return render_template("install.html", client_id=client_id, scope=scope)
 
 
-@app.route("/thanks", methods=["GET"])
+@app.route("/thanks", methods=["GET", "POST"])
 def thanks():
-    lunchBot.runner()
+    print('current jobs: ', jobs)
+    # lunchBot.runner()
+    w = Thread(target=invoke_watcher)
+    w.start()
+    print('watcher invoked thanks route')
     return render_template("thanks.html")
 
 
+def invoke_watcher():
+    # global jobs
+    print('invoke watcher jobs: ', jobs)
+    print('watcher invoked invoke_function')
+    # completed_jobs = {}
+    while True:
+        print('jobs inside while loop: ', jobs)
+        # if not completed_jobs:
+        for index, job in enumerate(jobs, start=1):
+            print('job in watcher: ', job)
+            gmt_plus_one = datetime.now() + timedelta(hours=1)
+            # current_time = time.strftime("%H:%M")
+            current_time = "{:%H:%M}".format(gmt_plus_one)
+            # current_time = 3
+            print(job.runtime(), current_time)
+            if str(current_time) == job.runtime():
+                job.runner()
+            # completed_jobs[index] = jobs
+
+        print('jobs after for loop: ', jobs)
+        # time.sleep(3600)
+        time.sleep(20)
+        print('finished first sleep')
+        time.sleep(20)
+        print('finished second sleep')
+        time.sleep(20)
+        print('finished third sleep')
+        # completed_jobs = {}
+
+
+def start_server():
+    print('starting server')
+    app.run()
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    s = Thread(target=start_server)
+
+    s.start()
+    # app.run()
