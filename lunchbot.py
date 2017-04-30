3bf80ff022dd56f2878a472a30552cc54cc55362
Chiemeka Alim <lord.chiemy@gmail.com>
2017-04-30 10:28:01 +0100

    WIP auth

--- lunchbot.py
+++ lunchbot.py
@@ -1,6 +1,10 @@
 import os
 from slackclient import SlackClient
 
+# TO-DO
+# http://api.slack.com/docs/oauth-token-safety
+authed_teams = {}
+
 
 class Bot(object):
     def __init(self):
@@ -11,3 +15,26 @@ class Bot(object):
         }
         self.verification = os.environ.get("VERIFICATION_TOKEN")
         self.client = SlackClient("")
+
+    def auth(self, code):
+        auth_response = self.client.api_call(
+            "oauth.access",
+            client_id=self.oauth["client_id"],
+            client_secret=self.oauth["client_secret"],
+            code=code
+        )
+        team_id = auth_response["team_id"]
+        authed_teams[team_id] = {
+            "bot_token": auth_response["bot"]["bot_access_token"]
+        }
+        self.client = SlackClient(authed_teams[team_id]["bot_token"])
+
+        # returns a dictionary of channels, users and the api_call status
+        channel_list = self.client.api_call("channels.list", exclude_archived=1)
+        users_list = self.client.api_call("users.list")
+        print('channel_list: ', channel_list)
+        print('users_list: ', users_list)
+
+        # get exact list of channels and users
+        self.actual_channel_list = channel_list["channels"]
+        self.actual_users = users_list["members"]
--- slack.py
+++ slack.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import lunchbot
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Running on heroku'
+    """
+    Renders installation page with "Add to Slack" button
+    """
+    # Bot = lunchbot.Bot()
+    client_id = lunchbot.oauth["client_id"]
+    print(client_id)
+    scope = lunchbot.oauth["scope"]
+
+    return render_template("install.html", client_id=client_id, scope=scope)
 
 
-if __name__ == '__main__':
+@app.route("/thanks")
+def thanks():
+    """
+    Exchange temp auth code for OAuth token
+    Render a thank you page
+    """
+    # Get temp auth code from request params
+    code_arg = request.args.get('code')
+
+    # The bot's auth method that exchanges code for OAuth token
+    lunchbot.auth(code_arg)
+    return redirect(url_for('thanks_page'))
+
+
+if __name__ == "__main__":
     app.run()
