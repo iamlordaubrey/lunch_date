6c74f1d529ee53c82779331f42cfc007b5c38d4b
Chiemeka Alim <lord.chiemy@gmail.com>
2017-05-01 01:16:58 +0100

    Testing cron

--- lunchbot.py
+++ lunchbot.py
@@ -146,7 +146,7 @@ class Bot(object):
     def runtime(self):
         # How often should this job run?
         # To-Do: Get runtime from the organization
-        return '01:14'
+        return '01:18'
 
     def runner(self):
         print('running...')
