87ef70c22d96c3d6a8aac7ebd718064dd17bfa8c
Chiemeka Alim <lord.chiemy@gmail.com>
2017-05-02 16:52:38 +0100

    Change runtime

--- lunchbot.py
+++ lunchbot.py
@@ -151,7 +151,7 @@ class Bot(object):
     def runtime(self):
         # How often should this job run?
         # To-Do: Get runtime from the organization
-        return '16:12'
+        return '17:05'
 
     def runner(self):
         print('running...')
