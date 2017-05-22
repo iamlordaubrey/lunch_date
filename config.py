6ee44e9f40420a5ff1cf93360290cdf34eb5bebb
Chiemeka Alim <lord.chiemy@gmail.com>
2017-05-22 08:36:48 +0100

    Create a config file

--- config.py
+++ config.py
@@ -14,14 +14,8 @@ class ProductionConfig(Config):
     DEBUG = False
 
 
-# class StagingConfig(Config):
-#     DEVELOPMENT = True
-#     DEBUG = True
-
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
-    # DEBUG = True
 
 
 class TestingConfig(Config):
